4852689b441e5c198dcb232bcb8fee6bb9e943b4
gilmujjang <alsanrlf@naver.com>
2021-07-03 15:06:27 +0900

    [ADD] 최근 가격을 주가에 담기

--- test.js
+++ test.js
@@ -5,12 +5,27 @@ import search from './search.js'
 // search("BTC")
 let count = 0;
 
+
+let btc_list = new Array(3)
+
+async function init_function(){
+  const coin_price_init = await search("BTC");
+  btc_list.fill(coin_price_init)
+}
+
+init_function();
+
+
 const loop = setInterval(async function() {
-  const coin_price = await search("ETH");
-  console.log(coin_price)
+  const coin_price = await search("BTC");
+  for (let i=btc_list.length-1; i>0; i--){
+    btc_list[i]= btc_list[i-1];
+  }
+  btc_list[0] = coin_price;
+  console.log(btc_list);
 
   count ++
-  if(count === 3){
-    clearInterval(loop)
+  if(count === 5){
+    clearInterval(loop);
   }
 },1000)
