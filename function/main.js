38a102e6b5a859ae3dba3caaab5a7eb0c933c624
gilmujjang <alsanrlf@naver.com>
2021-07-26 09:52:32 +0900

    [FIX]  매수시 소수점 에러 해결,함수구조 변경

--- function/main.js
+++ function/main.js
@@ -55,86 +55,94 @@ async function init_function(){
   }
 }
 
-init_function();
+async function main_function() {
+  // 최근가격 갱신
+const coins_price = await search_coins(target_coin_list);
+for(let i=0; i< lists_list.length; i++){
+  for (let j=lists_list[i].length-1; j>0; j--){
+    lists_list[i][j]= lists_list[i][j-1];
+  }
+  lists_list[i][0] = coins_price[i];
 
+  // 볼린저밴드 계산
+  const n = lists_list[i].length;
+  const Mean = Math.round(lists_list[i].reduce((a,b) => a+b,0) / n);
+  const Std = Math.round(Math.sqrt(lists_list[i].map(x => Math.pow(x - Mean,2)).reduce((a,b) => a+b)/n))
+  const bollinger_top = Mean + Std*sd;
+  const bollinger_bottom = Mean - Std*sd;
+  let high = 100000000;
+
+  num[i].push(count);
+  center[i].push(Mean);
+  top[i].push(bollinger_top);
+  bottom[i].push(bollinger_bottom);
+
+  if(target_coin_status[i] == true){
+    high = Math.max(...lists_list[i])
+  }
+
+  if(coins_price[i] > bollinger_top && target_coin_status[i] == false){
+    if(hold_coin_num==3){
+      console.log("보유코인3가지임 아무튼 에러임");
+      return
+    }
+    trade("buy",target_coin_list[i],((my_asset[0]/(3-hold_coin_num))/coins_price[i]).toFixed(5));
+    target_coin_status[i] = true;
+    hold_coin_num = hold_coin_num+1;
+    console.log(target_coin_list[i],"볼린저 상단 돌파");
+    my_asset = await balance(target_coin_list);
+  }
+  if(coins_price[i] < high*0.9 && target_coin_status[i] == true && coins_price[i] < bollinger_top){
+    my_asset_units = await balance_units(target_coin_list);
+    trade("sell",target_coin_list[i],my_asset_units[i]);
+    target_coin_status[i] = false;
+    hold_coin_num = hold_coin_num-1;
+    console.log(target_coin_list[i],"고가에서 10% 하락");
+    my_asset = await balance(target_coin_list);
+    my_asset_units = await balance_units(target_coin_list);
+  }
+  const time = moment().format('YYYYMMDDHHmmss');
+  const coin_price_obj = {
+    time: time,
+    price: coins_price[i],
+    center: Mean,
+    top: bollinger_top,
+    bottom: bollinger_bottom
+  }
+dbService.collection(target_coin_list[i]+"_price").doc(time).set(coin_price_obj)
+}
+count ++;
+console.log(count,"회 실행중");
+}
+
+async function daily_save(){
+  my_asset = await balance(target_coin_list);
+  my_asset_units = await balance_units(target_coin_list);
+  const time = moment().format('YYYYMMDDHHmmss');
+
+  const balance_obj = {
+    time: time,
+    cash: my_asset[0],
+    btc: my_asset[1],
+    eth: my_asset[2],
+    bnb: my_asset[3]
+  }
+  dbService.collection("balance").doc(time).set(balance_obj)
+  console.log("일일 계좌 저장")
+  console.log(my_asset);
+}
+
+init_function();
+main_function();
+daily_save();
 async function MainLoop() {
   // 시세 감시
   setInterval(async function() {
-    // 최근가격 갱신
-    const coins_price = await search_coins(target_coin_list);
-    for(let i=0; i< lists_list.length; i++){
-      for (let j=lists_list[i].length-1; j>0; j--){
-        lists_list[i][j]= lists_list[i][j-1];
-      }
-      lists_list[i][0] = coins_price[i];
-
-      // 볼린저밴드 계산
-      const n = lists_list[i].length;
-      const Mean = Math.round(lists_list[i].reduce((a,b) => a+b,0) / n);
-      const Std = Math.round(Math.sqrt(lists_list[i].map(x => Math.pow(x - Mean,2)).reduce((a,b) => a+b)/n))
-      const bollinger_top = Mean + Std*sd;
-      const bollinger_bottom = Mean - Std*sd;
-      let high = 100000000;
-
-      num[i].push(count);
-      center[i].push(Mean);
-      top[i].push(bollinger_top);
-      bottom[i].push(bollinger_bottom);
-
-
-      if(target_coin_status[i] == true){
-        high = Math.max(...lists_list[i])
-      }
-
-      if(coins_price[i] > bollinger_top && target_coin_status[i] == false){
-        if(hold_coin_num==3){
-          console.log("보유코인3가지임 아무튼 에러임");
-          return
-        }
-        trade("buy",target_coin_list[i],((my_asset[0]/(3-hold_coin_num))/coins_price[i]).toFixed(5));
-        target_coin_status[i] = true;
-        hold_coin_num = hold_coin_num+1;
-        console.log(target_coin_list[i],"볼린저 상단 돌파");
-        my_asset = await balance(target_coin_list);
-      }
-      if(coins_price[i] < high*0.9 && target_coin_status[i] == true && coins_price[i] < bollinger_top){
-        my_asset_units = await balance_units(target_coin_list);
-        trade("sell",target_coin_list[i],my_asset_units[i]);
-        target_coin_status[i] = false;
-        hold_coin_num = hold_coin_num-1;
-        console.log(target_coin_list[i],"고가에서 10% 하락");
-        my_asset = await balance(target_coin_list);
-        my_asset_units = await balance_units(target_coin_list);
-      }
-      const time = moment().format('YYYYMMDDHHmmss');
-      const coin_price_obj = {
-        time: time,
-        price: coins_price[i],
-        center: Mean,
-        top: bollinger_top,
-        bottom: bollinger_bottom
-      }
-      dbService.collection(target_coin_list[i]+"_price").doc(time).set(coin_price_obj)
-    }
-    count ++;
-    console.log(count,"회 실행중");
+    main_function();
   },3600000)
   // 계좌 조회
   setInterval(async function() {
-    my_asset = await balance(target_coin_list);
-    my_asset_units = await balance_units(target_coin_list);
-    const time = moment().format('YYYYMMDDHHmmss');
-
-    const balance_obj = {
-      time: time,
-      cash: my_asset[0],
-      btc: my_asset[1],
-      eth: my_asset[2],
-      bnb: my_asset[3]
-    }
-    dbService.collection("balance").doc(time).set(balance_obj)
-    console.log("일일 계좌 저장")
-    console.log(my_asset);
+    daily_save();
   },86400000)
 }
 
