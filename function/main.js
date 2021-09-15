d54180341ea64e78da801c0c4f85b1546dedc61b
gilmujjang <alsanrlf@naver.com>
2021-09-15 14:30:24 +0900

    [FIX] 10분단위로 코인가격을 갱신하는데 배열의 크기는 1시간을 기준으로 설정되 있었음

--- function/main.js
+++ function/main.js
@@ -10,11 +10,7 @@ let count = 6;
 const sd = 1.5;
 const ma = 480;  //20일 은 480시간
 
-const btc_list = new Array(ma);
-const eth_list = new Array(ma);
-const bnb_list = new Array(ma);
 const target_coin_list = ["BTC", "ETH","BNB"];
-const lists_list = [btc_list, eth_list,bnb_list];
 let target_coin_status = [false, false, false];
 let hold_coin_num = 0;
 let my_asset = new Array(4);
@@ -23,10 +19,7 @@ let my_asset_units = new Array(3);
 async function init_function(){
   //시작하면 일단 계좌잔액 저장하고 시작
   daily_save();
-  //최근 480시간의 가격 정보 저장
-  for(let i=0; i< lists_list.length; i++){
-    lists_list[i] = await candle_price(target_coin_list[i],"1h",ma)
-  }
+
   my_asset = await balance(target_coin_list);
   for(let i=0; i<3; i++){
     //코인이 만원 이상 있으면 보유하고있다고 판단
@@ -41,21 +34,16 @@ async function init_function(){
 }
 
 async function main_function() {
-    // 최근가격 갱신
-  const coins_price = await search_coins(target_coin_list);
-  for(let i=0; i< lists_list.length; i++){
-    for (let j=lists_list[i].length-1; j>0; j--){
-      lists_list[i][j]= lists_list[i][j-1];
-    }
-    lists_list[i][0] = coins_price[i];
-
+  for(let i=0; i< target_coin_list.length; i++){
+    // 최근가격 조회
+    const recentCoinPrice =  await candle_price(target_coin_list[i],"1h",ma)
     // 볼린저밴드 계산
-    const Mean = Math.round(lists_list[i].reduce((a,b) => a+b,0) / ma);
-    const Std = Math.round(Math.sqrt(lists_list[i].map(x => Math.pow(x - Mean,2)).reduce((a,b) => a+b)/ma))
+    const Mean = Math.round(recentCoinPrice.reduce((a,b) => a+b,0) / ma);
+    const Std = Math.round(Math.sqrt(recentCoinPrice.map(x => Math.pow(x - Mean,2)).reduce((a,b) => a+b)/ma))
     const bollinger_top = Mean + Std*sd;
-    const high = Math.max(...lists_list[i])
+    const high = Math.max(...recentCoinPrice)
     const donkeyonBottom = high*0.9;
-    const coinPrice = coins_price[i]
+    const coinPrice = recentCoinPrice[0]
 
     //볼린저상단을 돌파 & 미보유 코인 & 매도점 이상이면 매수
     if(coinPrice > bollinger_top && target_coin_status[i] == false && coinPrice > donkeyonBottom){
@@ -89,13 +77,12 @@ async function main_function() {
       const time = moment().format('YYYYMMDDHHmmss');
       const coin_price_obj = {
         time: time,
-        price: coins_price[i],
+        price: coinPrice,
         center: Mean,
         top: bollinger_top,
         bottom: donkeyonBottom
       }
-  
-      dbService.collection(target_coin_list[i]+"_price").doc(time).set(coin_price_obj)
+      saveCoinPrice(target_coin_list[i], coin_price_obj);
     }
   }
 
@@ -105,6 +92,11 @@ async function main_function() {
   
   count ++;
   //10분에 한번씩 실행하지만 1시간에 한번씩 코인 가격을 저장
+}
+
+function saveCoinPrice(targetCoin, coin_price_obj){
+  const time = moment().format('YYYYMMDDHHmmss');
+  dbService.collection(targetCoin+"_price").doc(time).set(coin_price_obj)
 }
 
 
@@ -119,7 +111,7 @@ async function daily_save(){
     bnb: my_asset[3]
   }
 
-  dbService.collection("balance").doc(time).set(balance_obj)
+  // dbService.collection("balance").doc(time).set(balance_obj)
   console.log("일일 계좌 저장")
   console.log(my_asset);
 }
